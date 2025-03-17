2a163a31c2ab878e9c901238e684866445b4ed6e
manju-app-developer <sciencephysics661@gmail.com>
2025-03-17 14:53:30 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "roamzyee-cache-v3"; // Updated cache version
+const CACHE_NAME = "roamzyee-cache-v3";
 const urlsToCache = [
     "/",
     "/index.html",
@@ -6,46 +6,35 @@ const urlsToCache = [
     "/style.css",
     "/logo.png",
     "/icon-192.png",
-    "/icon-512.png",
-    "/manifest.json"
+    "/icon-512.png"
 ];
 
-// Install Service Worker & Cache Essential Resources
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
-            console.log("Caching essential resources...");
             return cache.addAll(urlsToCache);
         })
     );
 });
 
-// Activate & Clean Up Old Caches
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
 self.addEventListener("activate", event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cache => {
                     if (cache !== CACHE_NAME) {
-                        console.log("Deleting old cache:", cache);
                         return caches.delete(cache);
                     }
                 })
             );
         })
-    );
-});
-
-// Network-First Strategy (Always Try Fetching Fresh Content)
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        fetch(event.request)
-            .then(response => {
-                return caches.open(CACHE_NAME).then(cache => {
-                    cache.put(event.request, response.clone());
-                    return response;
-                });
-            })
-            .catch(() => caches.match(event.request))
     );
 });
