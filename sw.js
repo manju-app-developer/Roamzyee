bc92c88ba0af0eda25aac767e351c2dd7ee5ac48
manju-app-developer <sciencephysics661@gmail.com>
2025-03-17 14:07:28 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -44,3 +44,29 @@ self.addEventListener('activate', event => {
         })
     );
 });
+const CACHE_NAME = "roamzyee-cache-v2";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/welcome.html",
+    "/style.css",
+    "/logo.png",
+    "/icon-192.png",
+    "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
